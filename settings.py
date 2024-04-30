4412998755ef755e439858062de8fd6b71f6ee59
Anas1010101 <anasouadid11@gmail.com>
2024-04-30 15:44:05 +0200

    Initalisation du fichier setting avec le variables globales

---
+++ settings.py
@@ -0,0 +1,15 @@
+from pygame.math import *
+#screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+TILE_SIZE = 64
+
+LAYERS = {
+	'water': 0,
+	'ground': 1,
+	'forest grass':2,
+	'outside decorations': 3,
+	'fence' : 5,
+	'main': 6,
+	'building': 7
+}
