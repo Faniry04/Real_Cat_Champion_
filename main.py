f1e8dd2f57933ee6234edd5756d828dffc89ccd8
Anas1010101 <anasouadid11@gmail.com>
2024-04-30 16:04:01 +0200

    Codage de l affichage de l' écran avec variable temporaire

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ class Game:
         self.clock = pygame.time.Clock()
 
 
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+
 if __name__ == '__main__':
     game = Game()
+    game.run()
