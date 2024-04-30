5b73278a25486318f238721265905b85177fe267
Faniry04 <faniry.andriamasinoro1@gmail.com>
2024-04-30 16:14:56 +0200

    Création de la méthode qui affiche les différents sprites dans le bon ordre (CameraGroup)

--- level.py
+++ level.py
@@ -8,3 +8,14 @@ class CameraGroup(pygame.sprite.Group):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
+        for layer in LAYERS.values():
+            for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+                if sprite.z == layer:
+                    offset_rect = sprite.rect.copy()
+                    offset_rect.center -= self.offset
+                    self.display_surface.blit(sprite.image, offset_rect)
