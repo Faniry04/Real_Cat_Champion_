443b1d603c57bc36e277644e56d6528576732e6f
Anas1010101 <anasouadid11@gmail.com>
2024-04-30 16:43:46 +0200

    Importation des assets et des sprites pour le projet

---
+++ import/data/Tilesets/Hospital.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Hospital" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="../../graphics/buildings/Hospital.png" trans="ff00ff" width="320" height="320"/>
+</tileset>
---
+++ import/data/Tilesets/Yellow House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Yellow House" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="../../graphics/buildings/Yellow House.png" trans="ff00ff" width="320" height="320"/>
+</tileset>
