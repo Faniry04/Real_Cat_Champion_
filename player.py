3795f4b1fc94b8613a3912bb04eed55884016d7b
Anas1010101 <anasouadid11@gmail.com>
2024-05-07 10:37:54 +0200

    Creation d'un systeme de mouvement avec les touches du clavier

--- player.py
+++ player.py
@@ -26,3 +26,43 @@ class Player(pygame.sprite.Sprite):
 
 		self.hitbox = self.rect.copy().inflate((-126, -70))
 		self.collision_sprites = collision_sprites
+
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+			self.status = 'up'
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+			self.status = 'down'
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+			self.status = 'right'
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+			self.status = 'left'
+		else:
+			self.direction.x = 0
+
+	def move(self,dt):
+
+		# normalizing a vector
+		if self.direction.magnitude() > 0:
+			self.direction = self.direction.normalize()
+
+		# horizontal movement
+		self.pos.x += self.direction.x * self.speed * dt
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
+
+		# vertical movement
+		self.pos.y += self.direction.y * self.speed * dt
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
