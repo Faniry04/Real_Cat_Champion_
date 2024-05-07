7e3de453c4c76b55ee2aebdd7d91e93f5961546a
Faniry04 <faniry.andriamasinoro1@gmail.com>
2024-05-07 15:04:46 +0200

    Codage de l'écran d'ouverture du jeu

--- main.py
+++ main.py
@@ -12,16 +12,32 @@ class Game:
         pygame.display.set_caption('Real Cat Champion')
         self.clock = pygame.time.Clock()
         self.level = Level()
+        self.game_active = 0
     def run(self):
-        while True:
+
+        while self.game_active ==0:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-            dt = self.clock.tick() / 1000
-            self.level.run(dt)
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.game_active = 1
+
+            ecran_start = pygame.image.load('import/graphics/ecrans/ecran_start.png').convert()
+            self.screen.blit(ecran_start, (0, 0))
             pygame.display.update()
+
+
+        while self.game_active == 1:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+
+                dt = self.clock.tick() / 1000
+                self.level.run(dt)
+                pygame.display.update()
 
 if __name__ == '__main__':
     game = Game()
